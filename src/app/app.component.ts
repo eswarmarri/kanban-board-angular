c5bef647627692351e8b673be3fff73bbe6513d9
eswarmarri <50416136+eswarmarri@users.noreply.github.com>
2024-12-14 22:27:16 +0530

    basic lists implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterOutlet } from '@angular/router';
+import data from '../assets/data.json'
+import { ListComponentComponent } from './Components/list-component/list-component.component';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MatButtonModule, MatIconModule, ListComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'kanban-board-angular';
+  kanbanData = data;
 }
