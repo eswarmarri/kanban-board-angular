98334d7b52a67f0a461cd7476798fa566dc060d5
eswarmarri <50416136+eswarmarri@users.noreply.github.com>
2024-12-15 21:27:57 +0530

    basic drag & drop implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,38 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterOutlet } from '@angular/router';
 import data from '../assets/data.json'
-import { ListComponentComponent } from './Components/list-component/list-component.component';
 import { MatButtonModule } from '@angular/material/button';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+  CdkDrag,
+  CdkDropList,
+  CdkDropListGroup,
+} from '@angular/cdk/drag-drop';
 
 
 @Component({
   selector: 'app-root',
-  imports: [MatButtonModule, MatIconModule, ListComponentComponent],
+  imports: [MatButtonModule, MatIconModule, CdkDropList, CdkDropListGroup, CdkDrag],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'kanban-board-angular';
   kanbanData = data;
+
+  drop(event: CdkDragDrop<{ name: string; description: string; }[]>) {
+    console.log(event)
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
 }
